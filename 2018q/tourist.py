89f04e75b3b5b8e8346e701faac869c7909f67a0
Yu Wang <yuwang1@wharton.upenn.edu>
2018-07-08 19:44:20 -0400

    q1 of hackercup

---
+++ 2018q/tourist.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 07 23:52:44 2018
+
+@author: yuwan
+"""
+
+import sys
+
+def main():
+    cases = int(sys.stdin.readline())
+    for case in range(cases):
+        n, k, v = [int(a) for a in sys.stdin.readline().split()]
+        l = []
+        for _ in range(n):
+            l.append(sys.stdin.readline().strip())
+        ns = range(k*(v-1),k*v)
+        ns = [i%n for i in ns]
+        ns = sorted(ns)
+        ans = [l[i] for i in ns]
+        ans = ' '.join(ans)
+        print 'Case #{}: {}'.format(case+1, ans)
+        
+main()
+        
